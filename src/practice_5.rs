c56c8186d6819944cd7a3e16854b981463f5592b
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2024-10-23 16:27:01 +0300

    delete something

--- src/practice_5.rs
+++ src/practice_5.rs
@@ -7,7 +7,7 @@ fn check_convert() {
 
     for i in 0..HEIGHT {
         for j in 0..WIDTH {
-            // Логіка для побудови рамки та діагоналей
+  
             if i == 0  i == HEIGHT - 1  j == 0  j == WIDTH - 1  j == i * 2 || j == WIDTH - 1 - i * 2 {
                 envelope.push('*');
             } else {
