700aa327635145bfb587a5349e8f715ead2a8cb3
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2024-10-23 15:48:14 +0300

    add practice 9

---
+++ src/practice_9.rs
@@ -0,0 +1,24 @@
+[#test]
+fn is_prime(n: u64) -> bool {
+    if n < 2 {
+        return false;
+    }
+    
+    for i in 2..=((n as f64).sqrt() as u64) {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    
+    true
+}
+
+fn check_isPrime() {
+    let number = 30; // Число для перевірки
+    if is_prime(number) {
+        println!("{} є простим числом.", number);
+    } else {
+        println!("{} не є простим числом.", number);
+    }
+}
+
